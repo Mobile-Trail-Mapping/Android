1b5ebde64f296dbccc5b8036e05ebcaf2c3a54cf
Eric Stokes <fernferret@gmail.com>
2011-01-18 07:34:56 -0500

    Refine styles

--- Android/src/com/brousalis/AddPoint.java
+++ Android/src/com/brousalis/AddPoint.java
@@ -2,12 +2,14 @@ package com.brousalis;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AddPoint extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_point);
+		TextView t = (TextView) findViewById(R.id.new_point_summary_title);
 	}
 
 }
