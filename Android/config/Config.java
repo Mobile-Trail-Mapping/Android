04adb9fd8b75fa382399aa78ef777a1908bedacb
Eric Stokes <fernferret@gmail.com>
2011-01-19 22:55:15 -0500

    Added build config

---
+++ Android/config/Config.java
@@ -0,0 +1,10 @@
+ /*****************************************************\
+ |           AUTO GEN FILE WARNING!!!                  |
+ | !!! Edit this file ONLY in config/Config.java !!!   |
+ | If you do modify this from within Eclipse your      |
+ | changes WILL BE LOST!!!                             |
+ \*****************************************************/
+public class Config
+{
+    public final static boolean LOGGING = @CONFIG.LOGGING@;
+}
