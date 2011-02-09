1a2b06f33c8c5b573bf39b43608bf1e600a1910b
Eric Stokes <fernferret@gmail.com>
2011-02-09 11:58:20 -0500

    Removed unneeded package

--- Android/src/com/brousalis/TrailPrefs.java
+++ Android/src/com/brousalis/TrailPrefs.java
@@ -6,7 +6,6 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceManager;
