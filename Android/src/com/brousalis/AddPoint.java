ad330e48a4ad282310c278fe4dcab59e4638f0ec
Eric Stokes <fernferret@gmail.com>
2011-02-16 01:42:14 -0500

    Refactor AddPoint

--- Android/src/com/brousalis/AddPoint.java
+++ Android/src/com/brousalis/AddPoint.java
@@ -42,12 +42,16 @@ public class AddPoint extends Activity {
 	
 	private Spinner mTrailPicker;
 	private Spinner mCategoryPicker;
+	
 	private ImageView mImagePreview;
+	
+	private EditText mDescription;
+	private EditText mTitle;
+	
 	private Button mSubmitButton;
 	private Button mPictureButton;
 	private Button mCancelButton;
-	private EditText mDescription;
-	private EditText mTitle;
+	private Button mRemovePicButton;
 	
 	private String mImageFilePath;
 	
@@ -73,6 +77,9 @@ public class AddPoint extends Activity {
 		mPictureButton = (Button) findViewById(R.id.add_picture_button);
 		mImagePreview = (ImageView) findViewById(R.id.picture_preview);
 		mTrailPicker = (Spinner) findViewById(R.id.new_point_trail);
+		mRemovePicButton = (Button) findViewById(R.id.remove_picture_button);
+		mCancelButton = (Button) findViewById(R.id.go_back_button);
+		mSubmitButton = (Button) findViewById(R.id.add_point_button);
 		
 		// Set the adapters for the Trails
 		ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, mTrailNames);
@@ -82,8 +89,6 @@ public class AddPoint extends Activity {
 		// Set the adapter for the Categories
 		mCategoryPicker = (Spinner) findViewById(R.id.new_point_category);
 		
-		mPictureButton.setOnClickListener(mOnAddPictureListener);
-		mImagePreview.setOnClickListener(mOnAddPictureListener);
 		// If we've rotated, we have a very cheap way to get the image again
 		// final Object data = getLastNonConfigurationInstance();
 		// if (data != null) {
@@ -91,11 +96,11 @@ public class AddPoint extends Activity {
 		// mImagePreview.setImageBitmap(mPicture);
 		// }
 		
-		mCancelButton = (Button) findViewById(R.id.go_back_button);
 		mCancelButton.setOnClickListener(mOnCancelListener);
-		
-		mSubmitButton = (Button) findViewById(R.id.add_point_button);
 		mSubmitButton.setOnClickListener(mOnSubmitPointListener);
+		mPictureButton.setOnClickListener(mOnAddPictureListener);
+		mImagePreview.setOnClickListener(mOnAddPictureListener);
+		mRemovePicButton.setOnClickListener(mOnRemovePictureListener);
 		
 		mSettings = PreferenceManager.getDefaultSharedPreferences(this);
 	}
@@ -103,13 +108,60 @@ public class AddPoint extends Activity {
 	private View.OnClickListener mOnAddPictureListener = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
-			
 			startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), SELECT_IMAGE);
 		}
 	};
 	
+	private View.OnClickListener mOnRemovePictureListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			mImageFilePath = null;
+			setPicturePreviewUnloaded();
+			mPicture.recycle();
+		}
+	};
+	private View.OnClickListener mOnCancelListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			finish();
+		}
+	};
+	private View.OnClickListener mOnSubmitPointListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			disableAllButtons();
+			mUploadProgress.setVisibility(View.VISIBLE);
+			createNewPoint();
+		}
+	};
+	
+	private void setPicturePreviewLoaded() {
+		mPictureButton.setText("Modify Picture");
+		mRemovePicButton.setVisibility(View.VISIBLE);
+	}
+	
+	private void setPicturePreviewUnloaded() {
+		mRemovePicButton.setVisibility(View.GONE);
+		mImagePreview.setImageDrawable(getResources().getDrawable(R.drawable.camera));
+		mPictureButton.setText("Add Picture");
+	}
+	
+	private void disableAllButtons() {
+		mDescription.setEnabled(false);
+		mTitle.setEnabled(false);
+		mImagePreview.setEnabled(false);
+		mTrailPicker.setEnabled(false);
+		mCategoryPicker.setEnabled(false);
+		mCancelButton.setEnabled(false);
+		mSubmitButton.setEnabled(false);
+		mImagePreview.setEnabled(false);
+		mRemovePicButton.setEnabled(false);
+		mPictureButton.setEnabled(false);
+	}
+	
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == SELECT_IMAGE && resultCode == Activity.RESULT_OK) {
+			setPicturePreviewLoaded();
 			mSelectedImageURI = data.getData();
 			mImageFilePath = NetUtils.getRealPathFromURI(mSelectedImageURI, this);
 			try {
@@ -140,47 +192,38 @@ public class AddPoint extends Activity {
 		NetUtils.postHTTPImage(otherValues, getString(R.string.actual_data_root) + getString(R.string.add_photo_path), mImageFilePath);
 	}
 	
-	private View.OnClickListener mOnSubmitPointListener = new View.OnClickListener() {
+	private void createNewPoint() {
 		
-		@Override
-		public void onClick(View v) {
-			HashMap<String, String> items = new HashMap<String, String>();
-			items.put("user", mSettings.getString(getString(R.string.key_username), ""));
-			items.put("pwhash", mSettings.getString(getString(R.string.key_password), ""));
-			
-			items.put("lat", convertIntGeoE6toFloat(mLocation.getLatitudeE6()) + "");
-			items.put("long", convertIntGeoE6toFloat(mLocation.getLongitudeE6()) + "");
-			
-			items.put("title", mTitle.getText().toString());
-			items.put("desc", mDescription.getText().toString());
-			items.put("trail", (String) mTrailPicker.getSelectedItem());
-			items.put("category", (String) mCategoryPicker.getSelectedItem());
-			Log.w(ShowMap.MTM, "hash: " + items.toString());
-			String result = NetUtils.postHTTPData(items, getString(R.string.actual_data_root) + getString(R.string.add_point_path));
-			Log.w(ShowMap.MTM, "Result: " + result);
-			// Try and retrieve the result, if it fails, the point failed adding, do not upload
-			if (mImageFilePath != null) {
-				try {
-					mID = Integer.parseInt(result);
-					mUploadProgress.setVisibility(View.VISIBLE);
-					new AsyncImageUploader().execute();
-				} catch (NumberFormatException e) {
-					mUploadProgress.setVisibility(View.INVISIBLE);
-					AlertDialog.Builder build = new AlertDialog.Builder(AddPoint.this);
-					build.setTitle("Failed to upload image").setMessage("The image was not uploaded. You can open the point and edit it later to add your photo.");
-				}
+		HashMap<String, String> items = new HashMap<String, String>();
+		items.put("user", mSettings.getString(getString(R.string.key_username), ""));
+		items.put("pwhash", mSettings.getString(getString(R.string.key_password), ""));
+		
+		items.put("lat", convertIntGeoE6toFloat(mLocation.getLatitudeE6()) + "");
+		items.put("long", convertIntGeoE6toFloat(mLocation.getLongitudeE6()) + "");
+		
+		items.put("title", mTitle.getText().toString());
+		items.put("desc", mDescription.getText().toString());
+		items.put("trail", (String) mTrailPicker.getSelectedItem());
+		items.put("category", (String) mCategoryPicker.getSelectedItem());
+		Log.w(ShowMap.MTM, "hash: " + items.toString());
+		String result = NetUtils.postHTTPData(items, getString(R.string.actual_data_root) + getString(R.string.add_point_path));
+		Log.w(ShowMap.MTM, "Result: " + result);
+		// Try and retrieve the result, if it fails, the point failed adding, do not upload
+		if (mImageFilePath != null) {
+			try {
+				mID = Integer.parseInt(result);
+				mUploadProgress.setVisibility(View.VISIBLE);
+				new AsyncImageUploader().execute();
+			} catch (NumberFormatException e) {
+				mUploadProgress.setVisibility(View.INVISIBLE);
+				AlertDialog.Builder build = new AlertDialog.Builder(AddPoint.this);
+				build.setTitle("Failed to upload image").setMessage("The image was not uploaded. You can open the point and edit it later to add your photo.");
 			}
+		} else {
 			finish();
 		}
-	};
-	
-	private View.OnClickListener mOnCancelListener = new View.OnClickListener() {
 		
-		@Override
-		public void onClick(View v) {
-			finish();
-		}
-	};
+	}
 	
 	/**
 	 * Used for converting android E6 values to the Server specified floats.
@@ -203,7 +246,6 @@ public class AddPoint extends Activity {
 		
 		@Override
 		protected void onPostExecute(Void result) {
-			mUploadProgress.setVisibility(View.INVISIBLE);
 			super.onPostExecute(result);
 			finish();
 		}
