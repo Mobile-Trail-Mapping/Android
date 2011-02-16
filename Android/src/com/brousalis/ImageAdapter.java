2ad1cb79a47bda95997b18998da52de17a3f835c
Eric Stokes <fernferret@gmail.com>
2011-02-16 03:00:34 -0500

    Added gallery scaling

--- Android/src/com/brousalis/ImageAdapter.java
+++ Android/src/com/brousalis/ImageAdapter.java
@@ -32,16 +32,17 @@ public class ImageAdapter extends BaseAdapter {
 	
 	private ArrayList<Bitmap> mDrawables;
 	private int mDensity;
+	
 	public ImageAdapter(Context c, int pointID, int numOfPictures, int density) {
 		WEB_FOLDER = c.getString(R.string.actual_data_root) + c.getString(R.string.photo_path);
 		mContext = c;
 		mGalleryImageFolder = DATA_FOLDER + pointID + "/";
 		mDensity = density;
 		// verifyImageCache(pointID);
-		TypedArray a = mContext.obtainStyledAttributes(R.styleable.DetailGallery);
+		//TypedArray a = mContext.obtainStyledAttributes(R.styleable.DetailGallery);
 		
-		mGalleryItemBackground = a.getResourceId(R.styleable.DetailGallery_android_galleryItemBackground, 0);
-		a.recycle();
+		//mGalleryItemBackground = a.getResourceId(R.styleable.DetailGallery_android_galleryItemBackground, 0);
+		//a.recycle();
 		mDrawables = new ArrayList<Bitmap>();
 		populateImages(numOfPictures);
 		Log.w(ShowMap.MTM, "Oncreate Fired");
@@ -82,20 +83,50 @@ public class ImageAdapter extends BaseAdapter {
 		Log.w(ShowMap.MTM, "using Recycled");
 		Bitmap b = mDrawables.get(position);
 		i.setImageBitmap(mDrawables.get(position));
+		int imgHeight = b.getHeight();
+		int imgWidth = b.getWidth();
 		
-		b.getHeight();
-		b.getWidth();
+		//determineImageScale(imgWidth, imgHeight, mDensity);
 		
-		
-		i.setLayoutParams(getLayoutParamsForScreen(mDensity));
+		i.setLayoutParams(determineImageScale(imgWidth, imgHeight, mDensity));
 		i.setScaleType(ImageView.ScaleType.FIT_CENTER);
 		i.setBackgroundResource(mGalleryItemBackground);
 		
 		return i;
 	}
 	
+	private Gallery.LayoutParams determineImageScale(int imgWidth, int imgHeight, int density) {
+		int finalWidth = imgWidth;
+		int finalHeight = imgHeight;
+		
+		int densityWidth = getLayoutParamsForScreen(density).width;
+		int densityHeight = getLayoutParamsForScreen(density).height;
+		Log.w(ShowMap.MTM, "Density Width: " + densityWidth);
+		Log.w(ShowMap.MTM, "Density Height: " + densityHeight);
+		int aspect;
+		if (imgHeight > densityHeight) {
+			aspect = Math.round((float) imgHeight / (float) imgWidth);
+			finalHeight = densityHeight;
+			finalWidth = Math.round((float)densityHeight / (float)aspect);
+			
+		} else if(imgWidth > densityWidth) {
+			aspect = Math.round((float) imgWidth / (float) imgHeight);
+			finalWidth = densityWidth;
+			finalHeight = Math.round((float)densityWidth / (float)aspect);
+			
+		}
+		// Very strange things going on with multiple densities, look into this
+		finalWidth += getGalleryOffsetForScreen(density);
+		Log.w(ShowMap.MTM, "Final Width: " + finalWidth);
+		Log.w(ShowMap.MTM, "Final Height: " + finalHeight);
+		// Add Width to account for image overlap
+		
+		return new Gallery.LayoutParams(finalWidth, finalHeight);
+		
+	}
+	
 	private Gallery.LayoutParams getLayoutParamsForScreen(int screenDPI) {
-		switch(screenDPI) {
+		switch (screenDPI) {
 			case DisplayMetrics.DENSITY_HIGH:
 				return new Gallery.LayoutParams(400, 300);
 			case DisplayMetrics.DENSITY_MEDIUM:
@@ -104,6 +135,23 @@ public class ImageAdapter extends BaseAdapter {
 				return new Gallery.LayoutParams(200, 150);
 			default:
 				return new Gallery.LayoutParams(300, 225);
+		}
+	}
+	
+	private int getGalleryOffsetForScreen(int screenDPI) {
+		switch (screenDPI) {
+			case DisplayMetrics.DENSITY_HIGH:
+				Log.w(ShowMap.MTM, "Found HDPI");
+				return 50;
+			case DisplayMetrics.DENSITY_MEDIUM:
+				Log.w(ShowMap.MTM, "Found MDPI");
+				return -30;
+			case DisplayMetrics.DENSITY_LOW:
+				Log.w(ShowMap.MTM, "Found LDPI");
+				return 0;
+			default:
+				Log.w(ShowMap.MTM, "Found Unknown DPI");
+				return 30;
 		}
 	}
 	
